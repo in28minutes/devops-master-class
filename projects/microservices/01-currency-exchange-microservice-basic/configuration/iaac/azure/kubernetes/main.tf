878ba414b51f5030010d58011ef16c07bc89815a
mehdielachqar <79844256+mehdielachqar@users.noreply.github.com>
2022-08-25 00:27:29 +0200

    Update main.tf

--- projects/microservices/01-currency-exchange-microservice-basic/configuration/iaac/azure/kubernetes/main.tf
+++ projects/microservices/01-currency-exchange-microservice-basic/configuration/iaac/azure/kubernetes/main.tf
@@ -25,7 +25,7 @@ resource "azurerm_kubernetes_cluster" "terraform-k8s" {
   default_node_pool {
     name            = "agentpool"
     node_count      = var.node_count
-    vm_size         = "standard_b2ms"
+    vm_size         = "Standard_D2_v2"
     # vm_size         = "standard_d2as_v5"      CHANGE IF AN ERROR ARISES 
   }
 
