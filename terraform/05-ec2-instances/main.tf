e0cadd850ab29c6066f98f78d81ad9090ea79e54
kahgeorge <88637241+kahgeorge@users.noreply.github.com>
2023-01-08 16:15:08 -0500

    update from hardcoding flexible

--- terraform/05-ec2-instances/main.tf
+++ terraform/05-ec2-instances/main.tf
@@ -1,6 +1,3 @@
-variable "aws_key_pair" {
-  default = "C:/Users/gemes/Downloads/terraform/aws_key_pair/default-ec2.pem"
-}
 provider "aws" {
   region = "us-east-1"
 }
@@ -11,24 +8,6 @@ resource "aws_default_vpc" "default" {
   }
 }
 
-data "aws_subnets" "default_subnets" {
-  filter {
-    name   = "vpc-id"
-    values = [aws_default_vpc.default.id]
-  }
-}
-
-data "aws_ami" "aws_linux_2_latest" {
-  most_recent = true
-  owners      = ["amazon"]
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm-*"]
-  }
-}
-data "aws_ami_ids" "aws_linux_2_latest_ids" {
-  owners = ["amazon"]
-}
 //HTTP Server -> SG
 //SG -> 80 TCP, 22 TCP, CIDR ["0.0.0.0/0"]
 resource "aws_security_group" "http_server_sg" {
---
+++ terraform/05-ec2-instances/variables.tf
@@ -0,0 +1,3 @@
+variable "aws_key_pair" {
+  default = "C:/Users/gemes/Downloads/terraform/aws_key_pair/default-ec2.pem"
+}
