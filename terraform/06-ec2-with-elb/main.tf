6c60dff28abd9fa6f7c8139b907c097d12070177
kahgeorge <88637241+kahgeorge@users.noreply.github.com>
2023-01-14 17:09:35 -0500

    update main

--- terraform/06-ec2-with-elb/main.tf
+++ terraform/06-ec2-with-elb/main.tf
@@ -6,8 +6,6 @@ resource "aws_default_vpc" "default" {
 
 }
 
-//HTTP Server -> SG
-//SG -> 80 TCP, 22 TCP, CIDR ["0.0.0.0/0"]
 resource "aws_security_group" "http_server_sg" {
   name = "http_server_sg"
   //vpc_id = "vpc-02523c91a94e96bca"
@@ -26,15 +24,37 @@ resource "aws_security_group" "http_server_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = -1
     cidr_blocks = ["0.0.0.0/0"]
   }
+
   tags = {
     name = "http_server_sg"
   }
+}
+
+resource "aws_security_group" "elb_sg" {
+  name   = "elb_sg"
+  vpc_id = aws_default_vpc.default.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
 }
 
 resource "aws_elb" "elb" {
@@ -58,12 +78,12 @@ resource "aws_instance" "http_servers" {
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.http_server_sg.id]
 
+  #for_each = data.aws_subnet_ids.default_subnets.ids
   for_each  = toset(data.aws_subnets.default_subnets.ids)
   subnet_id = each.value
 
   tags = {
     name : "http_servers_${each.value}"
-
   }
 
   connection {
@@ -77,7 +97,8 @@ resource "aws_instance" "http_servers" {
     inline = [
       "sudo yum install httpd -y",
       "sudo service httpd start",
-      "echo Welcome to in28minutes - virtual server is at ${self.public_dns} | sudo tee /var/www/html/index.html"
+      "echo Welcome to in28minutes - Virtual Server is at ${self.public_dns} | sudo tee /var/www/html/index.html"
     ]
   }
 }
+
--- terraform/06-ec2-with-elb/output.tf
+++ terraform/06-ec2-with-elb/output.tf
@@ -3,7 +3,7 @@ output "aws_security_group_http_server_details" {
 }
 
 output "http_server_public_dns" {
-  value = values(aws_instance.http_servers).*.id
+  value = aws_instance.http_servers.*.public_dns
 }
 
 output "elb_public_dns" {
